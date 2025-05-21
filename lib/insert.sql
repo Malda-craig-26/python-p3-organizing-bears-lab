bd598d94ad902799955b29621842cfeee0216f33
kimberly malda <maldakimberly228@gmail.com>
2025-05-21 09:16:54 +0300

    organizing bears

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name,sex,species,age) VALUES('Mr.Chocolate','M','Brown Bear',5);
+
+INSERT INTO bears (name,sex,species,age) VALUES('Rowdy','M','Polar Bear',7);
+
+INSERT INTO bears (name,sex,species,age) VALUES('Tabitha','F','Black Bear',4);
+
+INSERT INTO bears (name,sex,species,age) VALUES('Sergeant Brown','M','Sloth Bear',8);
+
+INSERT INTO bears (name,sex,species,age) VALUES('Melissa','F','Spectacled Bear',6);
+
+INSERT INTO bears (name,sex,species,age) VALUES('Grinch','M','Asiatic Bear',12);
+
+INSERT INTO bears (name,sex,species,age) VALUES('Wendy','F','Sun Bear',15);
+
+INSERT INTO bears (name,sex,species,age) VALUES(NULL,'M','Giant Panda',6);
